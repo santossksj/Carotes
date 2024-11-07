08aa0af1eb5d5d41b1fe66775843e29e0d4cd713
Luan Pedro <144036461+santossksj@users.noreply.github.com>
2024-11-07 09:37:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Programação',
+    'Oque é Python?',
+    'O Python linguagem de programação.',
+)
+
+criaCartao (
+    'Programação',
+    'Oque é uma Função?',
+    'Uma função é um bloco de código que executa alguma tarefa.',
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'É a cidade de Paris.',
+)
+
+criaCartao (
+    'Lingua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi (RAI)'
+)
